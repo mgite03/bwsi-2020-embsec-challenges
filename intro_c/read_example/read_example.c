fa8bdb7a1bf3ab3c066badd348e3f101832a7531
mgite03 <meghnagite@gmail.com>
2020-07-07 19:14:07 +0000

    7/7 work

--- intro_c/read_example/read_example.c
+++ intro_c/read_example/read_example.c
@@ -1,15 +1,31 @@
 
-// Include the uart header!
+// Include the uart header! - look at the uart code for more info
+#include "uart.h"
 
 int main(void)
 {
   // Variable setup
-
+    char flag[50];
+    int i;
+    
+    for(i = 0; i < 50; i ++){
+        flag[i] = 0;
+    }
+    
   // Initialize serial
-
+    uart_init(UART2); 
   // Read characters from serial into a string until a newline is received
-
+    i = 0;
+    int ret;
+    flag[i] = art_read(UART2, BLOCKING, &ret);
+    
+    while (flag[i] != '\n'){
+        i++;
+        flag[i] = uart_read(UART2, BLOCKING, &ret);
+    }
+    
   // Send the full string back over serial
-
-  return 0;
+    uart_write_str(UART2, flag);
+    
+    return 0;
 }
